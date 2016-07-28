954d7bc00de9be51a87854be616b8b796d01ba8d
Gagandeep Singh <gagan.singh04@gmail.com>
2016-07-28 09:47:08 -0700

    Review changes

--- arcgis-runtime-samples-osx/Maps/Map loaded/MapLoadedViewController.swift
+++ arcgis-runtime-samples-osx/Maps/Map loaded/MapLoadedViewController.swift
@@ -1,9 +1,17 @@
 //
-//  MapLoadedViewController.swift
-//  arcgis-runtime-samples-osx
+// Copyright 2016 Esri.
 //
-//  Created by Gagandeep Singh on 7/13/16.
-//  Copyright © 2016 Esri. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 import Cocoa
@@ -22,13 +30,15 @@ class MapLoadedViewController: NSViewController {
         //initialize map with basemap
         self.map = AGSMap(basemap: AGSBasemap.imageryWithLabelsBasemap())
         
-        //assign map to map view
-        self.mapView.map = self.map
-        
         //register as an observer for loadStatus property on map
         self.map.addObserver(self, forKeyPath: "loadStatus", options: .New, context: nil)
+        
+        //assign map to map view
+        self.mapView.map = self.map
     }
     
+    //The sample uses Key-Value Observing to register and receive observations on the loadStatus
+    //property of the AGSMap. The banner label will be updated everytime the status changes
     override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
         
         //update the banner label on main thread
